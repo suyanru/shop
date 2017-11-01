53ebb2b45914333af03aed1a783fab0ec26e1df8
suyanru <1021324363@qq.com>
2017-11-01 10:16:58 +0800

    123ffr

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
-const isProdMode = Object.is(process.env.NODE_ENV, 'production')
+const isPro = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-    baseUrl: isProdMode ? 'http://vnshop.shudong.wang/api/' : 'api/'
+    baseUrl: isPro ? 'http://suyanru.top/api/' : 'api/'
 }
